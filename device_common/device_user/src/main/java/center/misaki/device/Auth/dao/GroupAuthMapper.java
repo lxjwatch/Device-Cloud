f7b21bf0ee7c72c3e85dd5c65839779e38f5facc
showtimewwx <1255413402@qq.com>
2023-02-23 13:33:13 +0800

    [Bug修复]

--- device_common/device_user/src/main/java/center/misaki/device/Auth/dao/GroupAuthMapper.java
+++
@@ -1,9 +0,0 @@
-package center.misaki.device.Auth.dao;
-
-import center.misaki.device.domain.Pojo.GroupAuthForm;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface GroupAuthMapper extends BaseMapper<GroupAuthForm> {
-}
--- device_common/device_user/src/main/java/center/misaki/device/Auth/dao/UserAuthMapper.java
+++
@@ -1,9 +0,0 @@
-package center.misaki.device.Auth.dao;
-
-import center.misaki.device.domain.Pojo.UserAuthForm;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserAuthMapper extends BaseMapper<UserAuthForm> {
-}
